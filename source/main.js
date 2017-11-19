61e884df20b33709bb7534a82a210688508d8528
Chase Moskal <lordbrimshaw@gmail.com>
2017-11-19 10:02:16 -0800

    clean up main script

--- source/main.js
+++ source/main.js
@@ -1,43 +1,26 @@
 const standardGymWeightPairs = [
-	45,
-	45,
-	45,
-	45,
-	45,
-
-	35,
-	35,
-	35,
-	35,
-	35,
-
-	25,
-	25,
-	25,
-	25,
-	25,
-	25,
-
-	10,
-	10,
-	10,
-	10,
-	10,
-
-	5,
-	5,
-	5,
-	5,
-	5,
-
+	45, 45, 45, 45,
+	35, 35, 35, 35,
+	25, 25, 25, 25,
+	10, 10, 10, 10,
+	5, 5, 5, 5,
 	2.5
 ]
 
-function calculateWeights({goal, weightPairs = standardGymWeightPairs, barWeight = 45} ) {
+function sum(numbers) {
+	return numbers.reduce((a, b) => a + b, 0)
+}
+
+function calculateWeights({
+	goal,
+	weightPairs = standardGymWeightPairs,
+	barWeight = 45
+} ) {
+
 	let stack = []
 
 	for (const weight of weightPairs) {
-		const alreadyStackedWeight = stack.reduce((a, b) => a + b, 0) * 2
+		const alreadyStackedWeight = sum(stack) * 2
 		const proposedWeightToAdd = weight * 2
 
 		if ((barWeight + alreadyStackedWeight + proposedWeightToAdd) <= goal) {
@@ -45,7 +28,7 @@ function calculateWeights({goal, weightPairs = standardGymWeightPairs, barWeight
 		}
 	}
 
-	const achievement = barWeight + (stack.reduce((a, b) => a + b, 0) * 2)
+	const achievement = barWeight + (sum(stack) * 2)
 	return {stack, achievement}
 }
 
